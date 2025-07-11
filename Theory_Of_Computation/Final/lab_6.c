0246a2ce2121676b3aff1b63b53ef248286e0333
RAGREON <ragreon809@gmail.com>
2025-07-11 10:05:53 +0545

    added comments

--- Theory_Of_Computation/Final/lab_6.c
+++ Theory_Of_Computation/Final/lab_6.c
@@ -1,5 +1,6 @@
+// C-Program to check whether a string is a valid floating point number
+
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
 int isValidFloatingNumber(char* string) {
@@ -7,9 +8,13 @@ int isValidFloatingNumber(char* string) {
 
   while (*chPtr != '\0') {
     if (!isdigit(*chPtr)) {
+      // checks if the non-digit character is '.'
+      // returns false if char != '.'
+
       if (*chPtr != '.')
         return 0;
-      
+
+      // if there isn't any digit after '.' return false
       if (*(chPtr + 1) == '\0')
         return 0;
     }
@@ -21,10 +26,12 @@ int isValidFloatingNumber(char* string) {
 }
 
 int main(int argc, char** argv) {
+  // input string
   char string[50];
   printf("Enter a string: ");
   scanf("%s", string);
 
+  // checks if string is a valid floating point number or not
   printf("%s: %s floating point number", string, isValidFloatingNumber(string) ? "valid" : "invalid");
 
   return 0;
